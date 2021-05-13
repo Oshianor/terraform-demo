fcf3e38ada02c9cfafa77a5a17d3d7af9eebffcb
Abundance Oshianor <abundanceoshianor@gmail.com>
2021-05-13 23:19:38 +0100

    backup

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "my_pubic_key_location" {}
+variable "my_private_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -107,7 +108,7 @@ output "ec2_public_ip" {
 
 
 resource "aws_key_pair" "ssh-key" {
-  key_name = "server-key1"
+  key_name = "server-key"
   public_key = file(var.my_pubic_key_location)
 }
 
@@ -124,7 +125,28 @@ resource "aws_instance" "myapp-server" {
 
   key_name = aws_key_pair.ssh-key.key_name
 
-  user_data = file("entry-script.sh")
+  # user_data = file("entry-script.sh")
+
+
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
+    private_key = file(var.my_private_key_location)
+  }
+
+  provisioner "file" {
+    source = "entry-script.sh"
+    destination = "/home/ec2-user/entry-script-local.sh"
+  }
+
+  provisioner "remote-exec" {
+    script = file("entry-script-local.sh")
+  }
+  
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip}"
+  }
 
   tags = {
     Name : "${var.env_prefix}-server"
